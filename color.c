3e0137f88d18d002f3d47be2e28c1344ef32c9cb
Huy Pham <keixi512@gmail.com>
2024-11-07 10:54:27 +0700

    Gamma correction

--- color.c
+++ color.c
@@ -2,10 +2,20 @@
 #include "interval.h"
 #include <stdio.h>
 
+double linear_to_gamma(double linear_component) {
+    if (linear_component > 0)
+        return sqrt(linear_component);
+    return 0;
+}
+
 void write_color(color pixel_color) {
     double r = pixel_color.x;
     double g = pixel_color.y;
     double b = pixel_color.z;
+
+    r = linear_to_gamma(r);
+    b = linear_to_gamma(b);
+    g = linear_to_gamma(g);
 
     static interval intensity = {0.000, 0.999};
     int rbyte = (int)(256 * interval_clamp(&intensity, r));
